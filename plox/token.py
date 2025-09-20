1bdd6a629ec489706af3bf99f9d5f2ee1b7cb66d
Devanshusisodiya <devanshukumar45@gmail.com>
2025-09-21 00:26:56 +0530

    feat: added token types

---
+++ plox/token.py
@@ -0,0 +1,18 @@
+import typing as t
+from plox.token_type import TokenType
+
+class Token:
+    def __init__(
+        self, 
+        type: TokenType, 
+        lexeme: str,
+        literal: t.Dict,
+        line: int
+    ):
+        self.type = type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+    def to_string(self) -> str:
+        return self.type + " " + self.lexeme + " " + self.literal
